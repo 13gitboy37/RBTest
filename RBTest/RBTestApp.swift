a02cf0e586b4b2731cc2f3c00101230815ef98c1
Moshentsev Nikita <trane0909@bk.ru>
2024-01-23 20:44:23 +0300

    create project

---
+++ RBTest/RBTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RBTestApp.swift
+//  RBTest
+//
+//  Created by Никита Мошенцев on 23.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct RBTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
